f7a9dc7ba6b6a9a7a3589480582798d094712310
Semyon Dyachenko <semyondyachenko@gmail.com>
2023-03-05 19:33:23 +0300

    Update settings.py

--- foodleserver/settings.py
+++ foodleserver/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qh0530wm0g5-sk6ekc362y1$81eh9ph)j+w#*8#bgwtb!g1ylk'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
